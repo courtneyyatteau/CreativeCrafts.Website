83d0e529f05588980c9c681fd7270feabed487aa
Courtney Yatteau <courtney.yatteau@gmail.com>
2022-04-13 08:42:37 -0400

    Initial Commit

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using CreativeCrafts.Website.Models;
+using CreativeCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreativeCrafts.Website.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery]string ProductId, [FromQuery]int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using CreativeCrafts.Website.Models;
+using CreativeCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CreativeCrafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
+        {
+            _logger = logger;
+            ProductService = productService;   
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
